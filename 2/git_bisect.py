f71541b5c0c6b37b29f2ad76e949eb267d3f25d8
Ondotteess <108961850+Ondotteess@users.noreply.github.com>
2023-02-20 02:11:16 +0700

    Create git_bisect.py

---
+++ 2/git_bisect.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import subprocess
+import os
+import sys
+
+PATH = input('PATH: ')
+start = input('FIRST COMMIT: ')
+end = input('LAST COMMIT: ')
+TEST_CMD = input('TEST COMMAND: ')
+TEST_OUT = input('TEST OUTPUT: ')
+
+os.chdir(PATH)
+commit_list = subprocess.run("git log --format='%H'", capture_output=True)\
+                        .stdout.decode()\
+                        .replace("'", '').strip('\n')\
+                        .split('\n')[::-1]
+
+left = commit_list.index(start)
+right = commit_list.index(end)
+
+while left <= right:
+    mid = (left + right) // 2
+    subprocess.run(f'git checkout {commit_list[mid]}')
+    output = subprocess.run(TEST_CMD, capture_output=True).stdout.decode().strip('\r\n')
+    if output != TEST_OUT:
+        subprocess.run(f'git checkout {commit_list[mid-1]}')
+        if subprocess.run(TEST_CMD, capture_output=True).stdout.decode().strip('\r\n') == TEST_OUT:
+            print(f'Broken: {commit_list[mid-1]}')
+            exit(0)
+        else:
+            right = mid - 1
+
+    if output == TEST_OUT:
+        subprocess.run(f'git checkout {commit_list[mid + 1]}')
+        if subprocess.run(TEST_CMD, capture_output=True).stdout.decode().strip('\r\n') != TEST_OUT:
+            print(f'Broken: {commit_list[mid+1]}')
+            exit(0)
+        else:
+            left = mid + 1
