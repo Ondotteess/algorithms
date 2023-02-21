b7bcadf0ae2267cbfc20393bf3a28db490bd9174
Ondotteess <108961850+Ondotteess@users.noreply.github.com>
2023-02-22 02:52:43 +0700

    Update git_bisect.py

--- 2/git_bisect.py
+++ 2/git_bisect.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import subprocess
 import os
-import sys
 
 PATH = input('PATH: ')
 start = input('FIRST COMMIT: ')
@@ -14,27 +13,24 @@ commit_list = subprocess.run("git log --format='%H'", capture_output=True)\
                         .replace("'", '').strip('\n')\
                         .split('\n')[::-1]
 
+
 left = commit_list.index(start)
 right = commit_list.index(end)
+global mid
 
 while left <= right:
     mid = (left + right) // 2
     subprocess.run(f'git checkout {commit_list[mid]}')
     output = subprocess.run(TEST_CMD, capture_output=True).returncode
-    if output != 0:
-        subprocess.run(f'git checkout {commit_list[mid-1]}')
-        if subprocess.run(TEST_CMD, capture_output=True).returncode == 0:
-            print(f'Broken: {commit_list[mid-1]}')
-            subprocess.run(f'git checkout {commit_list[-1]}')
-            exit(0)
-        else:
-            right = mid - 1
 
-    if output == 0:
-        subprocess.run(f'git checkout {commit_list[mid + 1]}')
-        if subprocess.run(TEST_CMD, capture_output=True).returncode != 0:
-            print(f'Broken: {commit_list[mid+1]}')
-            subprocess.run(f'git checkout {commit_list[-1]}')
-            exit(0)
-        else:
-            left = mid + 1
+    right = mid - 1 if output else right
+    left = mid + 1 if not output else left
+
+output = subprocess.run(TEST_CMD, capture_output=True).returncode
+
+if output:
+    print(f'Broken: {commit_list[mid]}')
+else:
+    print(f'Broken: {commit_list[mid-1]}')
+
+subprocess.run(f'git checkout {commit_list[-1]}')
