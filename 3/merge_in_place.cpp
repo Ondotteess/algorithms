9291021e8993c2db3ed005a94249d725375ba260
Ondotteess <108961850+Ondotteess@users.noreply.github.com>
2023-03-13 21:42:43 +0700

    Update merge_in_place.cpp

--- 3/merge_in_place.cpp
+++ 3/merge_in_place.cpp
@@ -1,52 +1,46 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-void merge(int arr[], int start, int mid, int end) {
-    int start2 = mid + 1;
-    if (arr[mid] <= arr[start2]) {
-        return;
-    }
-    while (start <= mid && start2 <= end) {
-        if (arr[start] <= arr[start2]) {
-            start++;
-        } else {
-            int value = arr[start2];
-            int index = start2;
 
-            while (index != start) {
-                arr[index] = arr[index - 1];
-                index--;
+void merge(vector<int>& arr, int left, int mid, int right) {
+    int i = left, j = mid + 1, k = left;
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j]) {
+            i++;
+        }
+        else {
+            for (int l = j; l > i; l--) {
+                swap(arr[l], arr[l - 1]);
             }
-            arr[start] = value;
-            start++;
+            i++;
+            j++;
             mid++;
-            start2++;
         }
     }
 }
 
-void mergeSort(int arr[], int l, int r) {
-    if (l < r) {
-        int m = l + (r - l) / 2;
-
-        mergeSort(arr, l, m);
-        mergeSort(arr, m + 1, r);
-
-        merge(arr, l, m, r);
+void mergeSort(vector<int>& arr, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
     }
 }
 
-void printArray(int A[], int size)
+void printArray(vector<int> arr, int size)
 {
     int i;
     for (i = 0; i < size; i++)
-        cout << " " << A[i];
+        cout << " " << arr[i];
     cout << "\n";
 }
 
 int main() {
-    int arr[] = { -2, 3, -5, 6234623, 67345, 4346, -56,73,4,-47,-68,  6,87, 46, 579, 55,21, -35,-4 ,7,45, -4572345 ,-46, 0, 0, 3, 78, -64, -3958, -6, 1, -6, -457 };
-    int arr_size = sizeof(arr) / sizeof(arr[0]);
+    vector<int> arr = { -2, 3, -5, 6234623, 67345, 4346, -56,73,4,-47,-68,  6,87, 46, 579, 55,21, -35,-4 ,7,45, -4572345 ,-46, 0, 0, 3, 78, -64, -3958, -6, 1, -6, -457 };
+    int arr_size = arr.size();
 
     mergeSort(arr, 0, arr_size - 1);
 
