ac3dcdebacddc9f55ede2d9cec3a1da851acadec
Ondotteess <108961850+Ondotteess@users.noreply.github.com>
2023-03-22 16:17:24 +0700

    Update netherlands.cpp

--- 4/netherlands.cpp
+++ 4/netherlands.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void netherlands(vector<int> nums) {
+vector<int> netherlands(vector<int> nums) {
     int n = nums.size(), low, mid, high;
     low = mid = 0;
     high = n - 1;
@@ -13,14 +13,18 @@ void netherlands(vector<int> nums) {
         else if (nums[mid] == 1) mid++;
 
         else if (nums[mid] == 2) swap(nums[mid], nums[high--]);
+        
+    }
+
+    return nums;
 }
-  
+
 int main(){
   vector<int> array1 = { 0, 2, 2, 2, 0, 1, 0, 2, 1, 0, 2, 1, 2, 1, 0, 2, 0, 1, 0, 2, 2, 1, 0, 0, 2, 1, 2, 1, 0, 2, 2 };
   
-  niggerland(array1);
-  
-  for ( int x : array1 ){
-    cout << x << " "; 
-  }
+  array1 = netherlands(array1);
+   
+  for (int x : array1) {
+       cout << x << " ";
+   }
 }
