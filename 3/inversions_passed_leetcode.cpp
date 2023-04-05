022ba5c807f91c2ba451cd254a310adb8af2fda5
Ondotteess <108961850+Ondotteess@users.noreply.github.com>
2023-04-05 09:02:54 +0700

    Update inversions_passed_leetcode.cpp

--- 3/inversions_passed_leetcode.cpp
+++ 3/inversions_passed_leetcode.cpp
@@ -3,23 +3,18 @@
 
 using namespace std;
 
-bool check_sequence(vector<int> A) {
-    int i = 0;
-    while (i < A.size() - 1) {
-        if (A[i] > A[i + 1]) {
-            if (((i == 0) || (A[i - 1] < A[i])) && ((A[i] != i + 1) || (A[i + 1] != A[i] - 1))) {
+ bool isIdealPermutation(vector<int>& A) {
+        for(int i = 0 ; i < A.size() ; i++){
+            if(abs(A[i] - i) > 1)
                 return false;
-            }
         }
-        i++;
+        return true;
     }
-    return true;
-}
 
 int main() {
     vector<int> test_case1{ 1, 0, 2 };
     vector<int> test_case2{ 1, 2, 0 };
-    cout << check_sequence(test_case1) << endl; // Output: 1
-    cout << check_sequence(test_case2) << endl; // Output: 0
+    cout << isIdealPermutation(test_case1) << endl; // Output: 1
+    cout << isIdealPermutation(test_case2) << endl; // Output: 0
     return 0;
 }
