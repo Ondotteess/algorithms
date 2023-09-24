31e2f446e5514adcae7005188d9ac8af4070d3a9
Ondotteess <108961850+Ondotteess@users.noreply.github.com>
2023-09-24 22:34:38 +0300

    Update silly_timetable.cpp

--- 13/silly_timetable.cpp
+++ 13/silly_timetable.cpp
@@ -12,9 +12,76 @@ public:
     Task(char a, int b, int c) : name(a), deadline(b), fine(c) {
     }
 
-   
+
     bool operator<(const Task& other) const {
         return fine < other.fine;
+    }
+};
+
+class UnionFind {
+public:
+    vector<char> parent;
+    vector<int> rank;
+    vector<int> frees;
+
+    void print_parent() {
+        for (int i = 0; i < parent.size(); i++) {
+            cout << parent[i] << " ";
+        }
+        cout << "\n";
+    }
+
+    void print_rank() {
+        for (int i = 0; i < parent.size(); i++) {
+            cout << rank[i] << " ";
+        }
+        cout << "\n";
+    }
+
+    void print_frees() {
+        for (int i = 0; i < parent.size(); i++) {
+            cout << frees[i] << " ";
+        }
+        cout << "\n";
+    }
+                
+
+    UnionFind(int size) {
+        parent.resize(size);
+        rank.resize(size, 0);
+        frees.resize(size);
+        frees[0] = 0;
+        frees[1] = 0;
+        for (int i = 0; i < size; i++) {
+            if (i != 0) frees[i] = i-1;
+        }
+
+    }
+
+    void add(Task t) {
+        
+    }
+
+    int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    void unite(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+            }
+            else if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+            }
+            else {
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
+        }
     }
 };
 
@@ -47,7 +114,8 @@ public:
             if (table[i].name == ' ') {
                 table[i] = t;
                 return;
-            } else {
+            }
+            else {
                 i--;
             }
         }
@@ -63,29 +131,53 @@ public:
     }
 };
 
+class Timetable_U {
+public:
+    vector<Task> table;
+
+    Timetable_U(int size) {
+        table = vector<Task>(size, Task(' ', 0, 0));
+        table[0] = Task('-', 0, 0);
+    }
+
+};
+
 int main() {
-    
+
+    /*
+        СДЕЛАТЬ УФ ПО ДЕДЛАЙНАМ
+        ДЛЯ КАЖДОГО КЛАССА СОХРАНЯТЬ БЛИЖАЙШЕЕ СВОБОДНОЕ МЕСТО
+        -ВЕКТОР
+    */
+
+    UnionFind uf(5);
+  
+
     priority_queue<Task> taskQueue;
     Timetable tt(10);
-
+    
     taskQueue.push(Task('A', 3, 25));
     taskQueue.push(Task('B', 4, 10));
     taskQueue.push(Task('C', 1, 30));
     taskQueue.push(Task('D', 3, 50));
     taskQueue.push(Task('E', 3, 20));
+    
 
-    while (!taskQueue.empty()) {
+
+
+    /*while (!taskQueue.empty()) {
         Task maxTask = taskQueue.top();
         taskQueue.pop();
-
+    
         if (tt.table[maxTask.deadline].name == ' ') {
             tt.table[maxTask.deadline] = maxTask;
         } else {
             tt.push_left(maxTask);
         }
     }
+    */
 
-    tt.print();
+
 
     return 0;
 }
